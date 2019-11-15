0aa48480e37021c3fece92b65907978fe2e96e5b
calm0815(kuwano) <kuwano.tomohiro455@mail.kyutech.jp>
2019-11-16 00:53:34 +0900

    change key

--- ROS/kinetic_installer.sh
+++ ROS/kinetic_installer.sh
@@ -65,7 +65,7 @@ fi
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-yes | sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+yes | sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 
 sudo apt install --force-yes -yV ros-${Version}-desktop-full
