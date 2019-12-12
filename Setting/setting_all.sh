092437b4a5095fa8253fbee7c54bb3b9ff7989d1
calm0815(kuwano) <kuwano.tomohiro455@mail.kyutech.jp>
2019-12-12 17:19:26 +0900

    Update setting_all.sh

--- Setting/setting_all.sh
+++ Setting/setting_all.sh
@@ -12,6 +12,6 @@ ln -s ~/dotfiles/.inputrc ~/
 ln -s ~/dotfiles/.latexmkrc ~/
 ln -s ~/dotfiles/.tmux.conf ~/
 
-source ${pwd_dir}/Setting/nocaps.sh
-source ${pwd_dir}/Setting/emacs.sh
-source ${pwd_dir}/Setting/vim.sh
+sh ${pwd_dir}/Setting/nocaps.sh
+sh ${pwd_dir}/Setting/emacs.sh
+sh ${pwd_dir}/Setting/vim.sh
